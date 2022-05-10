7c77b387d7f00a19cbcd207b0a6ce2865d5d27a4
Aitor <agmanterola@users.noreply.github.com>
2022-05-10 16:10:19 +0200

    Preparing DB insertion from streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,6 @@ streamlit.dataframe(my_data_row)
 # Allow the end user to add a fruit to the list
 fruit_choice_add = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('The user entered: ',fruit_choice_add)
+
+# This will not work correctly, but just go with it for now
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
